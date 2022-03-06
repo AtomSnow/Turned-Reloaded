2771772e31fa2fa7449de69655dc2505475d13e6
xhayper <hayper1919@gmail.com>
2022-03-06 12:21:58 +0700

    Porting...

--- src/main/java/io/github/changedmc/turned/world/TurnedSurfaceRulesData.java
+++ src/main/java/io/github/changedmc/turned/world/TurnedSurfaceRulesData.java
@@ -5,14 +5,16 @@ import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.levelgen.SurfaceRules;
 
 public class TurnedSurfaceRulesData {
-
     private static final SurfaceRules.RuleSource DARK_LATEX_BLOCK = makeStateRule(TurnedBlock.DARK_LATEX_BLOCK.get());
     private static final SurfaceRules.RuleSource HARMLESS_DARK_LATEX_BLOCK = makeStateRule(TurnedBlock.HARMLESS_DARK_LATEX_BLOCK.get());
 
     protected static SurfaceRules.RuleSource makeRules()
     {
         SurfaceRules.ConditionSource isAtOrAboveWaterLevel = SurfaceRules.waterBlockCheck(-1, 0);
-        return SurfaceRules.sequence(SurfaceRules.ifTrue(SurfaceRules.abovePreliminarySurface(), SurfaceRules.sequence(SurfaceRules.ifTrue(isAtOrAboveWaterLevel, HARMLESS_DARK_LATEX_BLOCK), DARK_LATEX_BLOCK)));
+        SurfaceRules.RuleSource grassSurface = SurfaceRules.sequence(SurfaceRules.ifTrue(isAtOrAboveWaterLevel, HARMLESS_DARK_LATEX_BLOCK), DARK_LATEX_BLOCK);
+        return SurfaceRules.sequence(
+                SurfaceRules.ifTrue(SurfaceRules.ON_FLOOR, grassSurface)
+        );
     }
 
     private static SurfaceRules.RuleSource makeStateRule(Block block)
